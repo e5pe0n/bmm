ffacf70c66ae199027a0b6f8b21fbdd923abd6a2
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-07-31 22:17:56 +0900

    wip: feat: create bookmark

--- backend/src/domain/bookmark.rs
+++ backend/src/domain/bookmark.rs
@@ -1,17 +1,19 @@
 use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
 use time::OffsetDateTime;
-use time::serde::iso8601;
+use time::serde::rfc3339;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, sqlx::Type)]
+#[sqlx(transparent)]
 pub struct BookmarkId(pub i32);
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct Bookmark {
     pub id: BookmarkId,
     pub title: String,
     pub url: String,
-    #[serde(with = "iso8601")]
+    #[serde(with = "rfc3339")]
     pub created_at: OffsetDateTime,
-    #[serde(with = "iso8601")]
+    #[serde(with = "rfc3339")]
     pub updated_at: OffsetDateTime,
 }
