59a840a323318fd36043ef2853d25d0b476d2137
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-08-05 22:31:46 +0900

    chore(fe): install tailwindcss

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,12 +3,13 @@ import bmmLogo from "/succulents-svgrepo-com.svg";
 
 function App() {
   return (
-    <>
-      <div>
-        <img src={bmmLogo} alt="bmm logo" />
+    <div className="p-8">
+      <div className="flex space-x-2">
+        <img className="w-12 h-12" src={bmmLogo} alt="bmm logo" />
+        <h1 className="text-4xl font-bold">bmm</h1>
       </div>
       <h1>Vite + React</h1>
-    </>
+    </div>
   );
 }
 
