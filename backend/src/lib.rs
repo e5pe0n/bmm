900f5a8005dedb7dc24e9a708e3c77ccb78bc8a6
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-08-30 15:30:27 +0900

    ref(be): tracing

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -2,10 +2,14 @@ use crate::controller::{
     bookmarks::{create_bookmark, delete_bookmarks, list_bookmarks, update_bookmark},
     tags::{create_tag, delete_tags, list_tags, update_tag},
 };
-use axum::{Router, http::Method, routing::get};
+use axum::{Extension, Router, http::Method, routing::get};
 use sqlx::PgPool;
 use std::sync::Arc;
-use tower_http::cors::CorsLayer;
+use tower_http::{
+    cors::CorsLayer,
+    trace::{DefaultMakeSpan, TraceLayer},
+};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod app_state;
 mod controller;
@@ -16,18 +20,11 @@ mod repository;
 // Re-export AppState for use in main.rs
 pub use app_state::AppState;
 
-pub fn app(db: PgPool, ext_id: &str) -> Router {
+pub fn app(db: PgPool) -> Router {
     let shared_state = Arc::new(AppState {
         bookmark_repository: Box::new(repository::bookmark::BookmarkRepository::new(db.clone())),
         tag_repository: Box::new(repository::tag::TagRepository::new(db.clone())),
     });
-
-    let origins = [
-        "http://localhost:5173".parse().unwrap(),
-        ("chrome-extension://".to_string() + ext_id)
-            .parse()
-            .unwrap(),
-    ];
 
     Router::new()
         .route(
@@ -45,20 +42,4 @@ pub fn app(db: PgPool, ext_id: &str) -> Router {
                 .delete(delete_tags),
         )
         .with_state(shared_state)
-        .layer(
-            CorsLayer::new()
-                .allow_origin(origins)
-                .allow_methods([
-                    Method::GET,
-                    Method::DELETE,
-                    Method::POST,
-                    Method::PUT,
-                    Method::OPTIONS,
-                ])
-                .allow_headers([
-                    axum::http::header::CONTENT_TYPE,
-                    axum::http::header::AUTHORIZATION,
-                ])
-                .allow_credentials(true),
-        )
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,15 @@
 use anyhow::Context;
+use axum::http::{HeaderName, Method};
 use backend::app;
 use dotenvy::dotenv;
 use sqlx::postgres::PgPoolOptions;
 use std::env;
+use tower_http::{
+    cors::CorsLayer,
+    request_id::{MakeRequestUuid, PropagateRequestIdLayer, SetRequestIdLayer},
+    trace::{DefaultMakeSpan, TraceLayer},
+};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -16,7 +23,55 @@ async fn main() -> anyhow::Result<()> {
         .await
         .context("failed to connect to the database.")?;
 
-    let app = app(db, &ext_id);
+    let app = app(db);
+
+    let x_request_id = HeaderName::from_static("x-request-id");
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!(
+                    "{}=debug,tower_http=debug,axum::rejection=trace",
+                    env!("CARGO_CRATE_NAME")
+                )
+                .into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let origins = [
+        "http://localhost:5173".parse().unwrap(),
+        ("chrome-extension://".to_string() + &ext_id)
+            .parse()
+            .unwrap(),
+    ];
+
+    let app = app
+        .layer(
+            CorsLayer::new()
+                .allow_origin(origins)
+                .allow_methods([
+                    Method::GET,
+                    Method::DELETE,
+                    Method::POST,
+                    Method::PUT,
+                    Method::OPTIONS,
+                ])
+                .allow_headers([
+                    axum::http::header::CONTENT_TYPE,
+                    axum::http::header::AUTHORIZATION,
+                ])
+                .allow_credentials(true),
+        )
+        .layer(
+            TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::new().include_headers(true)),
+        )
+        .layer(SetRequestIdLayer::new(
+            x_request_id.clone(),
+            MakeRequestUuid::default(),
+        ))
+        .layer(PropagateRequestIdLayer::new(x_request_id));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
